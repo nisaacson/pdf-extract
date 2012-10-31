e00c2c4b7004dac7fa4cd2e9b222d87cae1f08fa
Noah Isaacson <noah@nisaacson.com>
2012-10-30 21:07:54 -0400

    switched to using ghostscript for pdf to tiff conversion

--- test/01_command-test.js
+++ test/01_command-test.js
@@ -4,10 +4,10 @@ var async = require('async');
 var exec = require('child_process').exec;
 
 describe('Command Test', function() {
-  it('should have convert binary on path', function(done) {
-    var cmd = 'which convert';
+  it('should have ghostscript (gs) binary on path', function(done) {
+    var cmd = 'which gs';
     var child = exec(cmd, function (err, stdout, stderr) {
-      should.not.exist(err, 'convert not available. You will not be able to perform ocr and extract text from pdfs with scanned image. To get convert install GraphicsMagick on your system');
+      should.not.exist(err, 'ghostscript not available. You will not be able to perform ocr and extract text from pdfs with scanned image. To get convert install GhostScript on your system');
       stderr.length.should.equal(0);
       should.exist(stdout);
       stdout.length.should.be.above(8);
